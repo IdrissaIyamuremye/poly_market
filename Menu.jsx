21664c6bbe85f06b724237fff7c6400e013d114e
IdrissaIyamuremye <idrissa.iyamuremye@ozu.edu.tr>
2026-01-11 02:05:05 +0300

    Added menu

--- Menu.jsx
+++ Menu.jsx
@@ -1,7 +1,7 @@
 
 function Menu() {
     return (
-        <div className="flex justify-end border-e text-xs text-white border-2 border-slate-300 h-12 ">
+        <div className="flex justify-end items-center border-e text-xs text-white border-2 border-slate-300 h-12 ">
             <div className="w-3/4 flex items-center justify-between gap-1">
                 <div className="basis-4 flex justify-center items-center px-10">Portfolio</div>
                 <div className="w-3/4">
@@ -14,14 +14,25 @@ function Menu() {
                 </div>
             </div>
             <div className="w-1/4">
-                <div className="">
-                    <div>
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                        <div></div>
+                <div className="flex items-center justify-between text-xs">
+                    <div className="basis-3/4 flex items-center justify-between">
+                        <div className="flex-col items-center border-2 border-white rounded-sm hover:opacity-50">
+                            <div className="text-slate-300 px-1 text-xs">Portfolio</div>
+                            <div className="text-center text-green-600 font-bold text-[10px]">$0.000</div>
+                        </div>
+                        <div>
+                            <div>Cash</div>
+                            <div>$000.00</div>
+                        </div>
+                        <div>
+                            <div>Deposit</div>
+                            <div>$000.00</div>
+                        </div>
+                        <div>
+                            <img src="" alt=""/>
+                        </div>
                     </div>
-                    <div>
+                    <div className="bais-1/4">
                         <div>profile pc</div>
                     </div>
                 </div>
