66ff36764ec820c0a1d4050c9a311facfb72cb19
IdrissaIyamuremye <idrissa.iyamuremye@ozu.edu.tr>
2026-01-12 02:12:49 +0300

    Modified Menu

--- Menu.jsx
+++ Menu.jsx
@@ -1,33 +1,39 @@
 
 function Menu() {
     return (
-        <div className="flex justify-end items-center border-e text-xs text-white border-2 border-slate-300 h-12 ">
+        <div className="flex justify-end items-center border-e text-xs text-white border-2 border-stone-500 h-12 ">
             <div className="w-3/4 flex items-center justify-between gap-1">
                 <div className="basis-4 flex justify-center items-center px-10">Portfolio</div>
-                <div className="w-3/4">
+                <div className="w-3/4 relative">
                     <input
                         type="text"
                         placeholder="Search Portfolio"
-                        className="w-3/4 h-6 outline-none p-2 text-slate-200 font-medium bg-transparent border-slate-300 border-2"
+                        className="w-3/4 h-8 outline-none text-xs pl-9 rounded-sm placeholder-slate-200 text-white bg-transparent border-stone-500 border-2 font-normal"
                     />
+                    <div className="absolute left-0 top-0 bottom-100% w-8 h-8 flex items-center justify-center">
+                        <span className="material-symbols-outlined cursor-pointer ">search</span>
+
+                    </div>
+
 
                 </div>
             </div>
             <div className="w-1/4">
                 <div className="flex items-center justify-between text-xs">
                     <div className="basis-3/4 flex items-center justify-between">
-                        <div className="flex-col items-center border-2 border-white rounded-sm hover:opacity-50">
+                        <div className="flex-col items-center border-none rounded-sm hover:bg-stone-500 cursor-pointer">
                             <div className="text-slate-300 px-1 text-xs">Portfolio</div>
                             <div className="text-center text-green-600 font-bold text-[10px]">$0.000</div>
                         </div>
-                        <div>
-                            <div>Cash</div>
-                            <div>$000.00</div>
+                        <div className="flex-col items-center border-none rounded-sm hover:bg-stone-500  cursor-pointer">
+                            <div className="text-slate-300 px-2 text-xs">Cash</div>
+                            <div className="text-center text-green-600 font-bold text-[10px]">$0.00</div>
                         </div>
-                        <div>
-                            <div>Deposit</div>
-                            <div>$000.00</div>
+                        <div className="flex-col items-center border-none rounded-sm hover:bg-stone-500  cursor-pointer">
+                            <div className="text-slate-300 px-2 text-xs">Cash</div>
+                            <div className="text-center text-green-600 font-bold text-[10px]">$0.00</div>
                         </div>
+
                         <div>
                             <img src="" alt=""/>
                         </div>
