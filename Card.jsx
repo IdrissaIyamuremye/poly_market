014a7a4018c2ef896773989f03a715482ec89347
IdrissaIyamuremye <idrissa.iyamuremye@ozu.edu.tr>
2026-01-14 00:27:12 +0300

    Add Butto hover

--- Card.jsx
+++ Card.jsx
@@ -4,14 +4,14 @@ import kahmenei from "./assets/Image/khamenei.webp"
 function Card(){
     return(
         <div className="w-full h-40 flex justify-between gap-2 border-2 border-white p-2">
-            <div className="bg-amber-700 h-full w-1/4 grid grid-cols-1 gap-2">
-                <div className="flex justify-between gap-1 h-10">
+            <div className="bg-amber-700 h-full w-1/4 grid grid-cols-1 gap-2 rounded-xl">
+                <div className="flex justify-between gap-4 h-10">
                     <div className="flex justify-start items-start h-full basis-1/4 ">
-                        <img src={kahmenei} className="h-10 w-10 rounded-sm" alt=""/>
+                        <img src={kahmenei} className="h-full w-full rounded-sm" alt=""/>
                     </div>
-                    <a href="#">
+                    <a href="#" className="hover:underline hover:text-white">
                         <div className="text-xs basis-3/5 font-bold text-white overflow-hidden line-clamp-2 min-w-auto">
-                            <p className="font-bold text-white line-clamp-2">Khamenei out as Supreme Leader of Iran by January 31?</p>
+                            <p className="font-bold text-white line-clamp-2 ">Khamenei out as Supreme Leader of Iran by January 31?</p>
                         </div>
                     </a>
                     <div className="basis-1/5">
@@ -38,8 +38,9 @@ function Card(){
                 </div>
                 <div className="flex justify-between gap-2">
                     <button className="h-auto w-full text-white text-sm font-bold cursor-pointer bg-yellow-900 hover:bg-yellow-400 rounded-sm">Yes</button>
-                    <button className="h-auto w-full text-white text-sm font-bold cursor-pointer bg-red-900 hover:bg-red-500 rounded-sm">No</button>
+                    <button className="h-auto w-full text-slate-200 text-sm font-bold cursor-pointer bg-red-900 hover:bg-red-500 rounded-sm">No</button>
                 </div>
+                {/*Yarın devam edecağm*/}
                 <div>end</div>
             </div>
             <div className="bg-white h-full w-1/4">2</div>
